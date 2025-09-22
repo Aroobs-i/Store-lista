458c83bb49cb534b694a1a2fd6de3456f4531c5d
Aroobs-i <iftikhar8995@gmail.com>
2025-09-22 17:09:52 +0500

    commit

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,9 +1,14 @@
 import Header from '@/components/Header'
 import MobileNavigation from '@/components/MobileNavigation'
 import SideBar from '@/components/SideBar'
+import { getCurrentUser } from '@/lib/actions/user.actions'
+import { redirect } from 'next/navigation'
 import React from 'react'
 
-const layout = ({ children }: { children: React.ReactNode}) => {
+const layout = async ({ children }: { children: React.ReactNode}) => {
+  const currentUser = await getCurrentUser();
+  if(!currentUser) return redirect('/sign-in');
+
   return (
     <main className='flex h-screen'>
         <SideBar/>
--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { navItems } from '@/constants'
+import { avatarPlaceholderUrl, navItems } from '@/constants'
 import { cn } from '@/lib/utils';
 import Image from 'next/image'
 import Link from 'next/link'
@@ -52,6 +52,27 @@ const SideBar = () => {
               ))}
             </ul>
         </nav>
+        <Image
+          src='/assets/images/files-2.png'
+          alt='files'
+          width={506}
+          height={418}
+          className='w-full'
+          />
+
+          <div className='sidebar-user-info'>
+            <Image
+              src={avatarPlaceholderUrl}
+              alt='avatar'
+              width={44}
+              height={44}
+              className='sidebar-user-avatar'
+              />
+              <div className='hidden lg:block'>
+                <p className='subtitle-2 capitalize'>{fullName}</p>
+                <p className='caption'>{email}</p>
+              </div>
+          </div>
     </aside>
   )
 }
