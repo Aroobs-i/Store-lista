75ac8e968da8195b59851a43523d828a0eeaa7ef
Aroobs-i <iftikhar8995@gmail.com>
2025-09-24 12:21:21 +0500

    commit

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,14 +4,16 @@ import Search from './Search'
 import FileUploader from './FileUploader'
 import { signOutUser } from '@/lib/actions/user.actions'
 
-const Header = () => {
+const Header = ({ userId , accountId }: { userId: string; accountId: string;}) => {
   return (
     <header className='header'>
         <Search/>
         <div className='header-wrapper'>
-            <FileUploader/>
-            <form action={async ()=>{
+            <FileUploader ownerId={userId} accountId={accountId}/>
+            <form 
+            action={async () => {
               'use server';
+
               await signOutUser();
             }}>
                 <button type='submit' className='sign-out-btn'>
