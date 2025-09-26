5c76d8627a0a19a165984f09e2def21b719abe18
Aroobs-i <iftikhar8995@gmail.com>
2025-09-26 11:13:34 +0500

    commit

--- components/ActionsModal.tsx
+++ components/ActionsModal.tsx
@@ -2,6 +2,7 @@ import { Models } from 'node-appwrite'
 import React from 'react'
 import Thumbnail from './Thumbnail'
 import FormattedDateTime from './FormattedDateTime'
+import { convertFileSize, formatDateTime } from '@/lib/utils'
 
 const ImageThumbnail = ({ file }: { file: Models.Document}) => (
     <div className='file-details-thumbnail'>
@@ -24,8 +25,8 @@ const ImageThumbnail = ({ file }: { file: Models.Document}) => (
 const DetailRow = ({label, value}: { label: string; value: string; }) => 
     (
     <div className='flex'>
-        <p className='file-details-label'>{label}</p>
-        <p className='file-details-value'>{value}</p>
+        <p className='file-details-label text-left'>{label}</p>
+        <p className='file-details-value text-left'>{value}</p>
     </div>
 
 )
@@ -34,7 +35,13 @@ export const FileDetails = ({ file }: { file: Models.Document}) => {
   return (
     <>
     <ImageThumbnail file={file} />
+    <div className='space-y-4 px-2 pt-2'>
     <DetailRow label='Format:' value={file.extension}/>
+    <DetailRow label='Size:' value={convertFileSize(file.size)}/>
+    <DetailRow label='Owner:' value={file.owner.fullName}/>
+    <DetailRow label='Last edit:' value={formatDateTime(file.$updatedAt)}/>
+    </div>
+
     </>
   )
 }
