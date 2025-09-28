f08706f6c8b7b35cf38c5b860ea82684dda0bd13
Aroobs-i <iftikhar8995@gmail.com>
2025-09-28 17:42:06 +0500

    Re-initialize repo

---
+++ app/(root)/page.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <div className="flex-center h-screen">
+      <h1 className="text-3xl text-brand font-bold">
+        StoreLista - The only storage solution you need.
+        </h1>
+
+    </div>
+  );
+}
---
+++ next.config.ts
@@ -0,0 +1,28 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  experimental:{
+    serverActions:{
+      bodySizeLimit: '100MB',
+    },
+  },
+  images:{
+
+    remotePatterns:[
+      {
+        protocol: "https",
+        hostname: "img.freepik.com"
+      },
+      {
+        protocol: "https",
+        hostname: "cdn.pixabay.com"
+      },
+      {
+        protocol: "https",
+        hostname: "fra.cloud.appwrite.io",
+      }
+    ]
+  }
+};
+
+export default nextConfig;
